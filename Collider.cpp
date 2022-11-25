3f68297616adf55954e11652bf34d962d7bf5768
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-25 17:09:20 +0900

    Colliderに衝突判定の中身を記載

--- Collider.cpp
+++ Collider.cpp
@@ -1,8 +1,26 @@
 #include "Collider.h"
+#include "Circle.h"
+#include "Line.h"
+#include "Vector2.h"
+#include <cmath>
 
 void Collider::Initialize(){
 }
 
-void Collider::Update(){
+void Collider::Update(Circle* circle,Line* line){
+	Vector2 distanceS = circle->pos - line->posS;
+	Vector2 lineVector = line->posE - line->posS;
 
+	lineVector = lineVector.Vec2Normalize();
+
+	float distanceSLtoC = 
+		distanceS.x * lineVector.y 
+		- lineVector.x  * distanceS.y;
+
+	if (fabs(distanceSLtoC)	< circle->r) {
+		isHit_ = true;
+	}
+	else {
+		isHit_ = false;
+	}
 }
--- Collider.h
+++ Collider.h
@@ -3,6 +3,9 @@
 class Collider {
 public:
 	void Initialize();
-	void Update();
+	void Update(Circle* circle,Line* line);
+
+	bool GetIsHit() const { return isHit_; }
 private:
+	bool isHit_ = false;
 };
